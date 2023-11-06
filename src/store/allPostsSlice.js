1c5abd82816aa7f22fc4df416c26806030bf6a4e
Rodrigo <digo.baiocchi@gmail.com>
2023-11-05 20:21:47 -0500

    creating redux slice for loading posts data

---
+++ src/store/allPostsSlice.js
@@ -0,0 +1,11 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const options = {
+    name: allPostsSlice,
+    initialState: [],
+    reducers: {
+        loadPosts: (state, payload) => {
+            return [action.payload]
+        }
+    }
+};
