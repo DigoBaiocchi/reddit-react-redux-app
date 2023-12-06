5609ac88cc60d80fa403ce793906106cd5700cce
Rodrigo <digo.baiocchi@gmail.com>
2023-12-05 20:47:24 -0500

    moving search input function to Header.jsx from App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,28 +8,6 @@ import { redditApi } from './api/redditApi';
 
 function App() {
   const [posts, setPosts] = useState([]);
-  const [searchInput, setSearchInput] = useState('');
-
-  const handleChange = ({ target }) => {
-    const { value } = target;
-    setSearchInput(value);
-    console.log(convertInput(searchInput));
-  }
-
-  const convertInput = (inputValue) => {
-    return inputValue.split('').map(char => {
-      const ascii = char.charCodeAt(0);
-      if (ascii === 32) {
-        return "%" + 20;
-      } else if ((ascii >= 65 && ascii <= 90)) {
-        return char;
-      } else if (ascii >= 97 && ascii <= 122) {
-        return char;
-      } else {
-        return "%" + ascii;
-      }
-    }).join('');
-  }
 
   const fetchApiData = () => {
     return fetch('https://www.reddit.com/.json')
@@ -51,7 +29,7 @@ function App() {
 
   return (
     <div>
-      <Header handleChange={handleChange} />
+      <Header />
       <main>
         <div>
           <div>
--- src/features/Header/Header.jsx
+++ src/features/Header/Header.jsx
@@ -3,6 +3,28 @@ import { useState } from 'react';
 import './Header.css'
 
 export const Header = (props) => {
+    const [searchInput, setSearchInput] = useState('');
+
+    const handleChange = ({ target }) => {
+        const { value } = target;
+        setSearchInput(value);
+        console.log(convertInput(searchInput));
+    }
+
+    const convertInput = (inputValue) => {
+        return inputValue.split('').map(char => {
+        const ascii = char.charCodeAt(0);
+        if (ascii === 32) {
+            return "%" + 20;
+        } else if ((ascii >= 65 && ascii <= 90)) {
+            return char;
+        } else if (ascii >= 97 && ascii <= 122) {
+            return char;
+        } else {
+            return "%" + ascii;
+        }
+        }).join('');
+    }
     return (
         <header>
             <nav className='nav-bar'>
@@ -11,7 +33,7 @@ export const Header = (props) => {
                 <a className='nav' href=''>New</a>
             </nav>
             <div className='search-section'>
-                <input id="search" className='search-input' onChange={props.handleChange}></input>
+                <input id="search" className='search-input' onChange={handleChange}></input>
                 <button className='search-button'>Search</button>
             </div>
         </header>    
