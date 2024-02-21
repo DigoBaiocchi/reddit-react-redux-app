111812cee55ea2e03b2ac643b3b03dabce2dc564
Rodrigo <digo.baiocchi@gmail.com>
2024-02-20 20:30:17 -0500

    Set reddit api to get specific page
    Setting reddit api function to get data from nav bar name when it is
    clicked

--- src/api/redditApi.js
+++ src/api/redditApi.js
@@ -1,9 +1,26 @@
-const BASE_URL = 'https://www.reddit.com/';
+const BASE_URL = 'https://www.reddit.com';
 
-export async function homePageRedditApi() {
-    const url = `${BASE_URL}.json`;
+export const redditPageApi = async (pageName) => {
+    let url = "";
+    switch (pageName) {
+        case "hot":
+            url = `${BASE_URL}/${pageName}/.json`;
+            break;
+        case "new":
+            url = `${BASE_URL}/${pageName}/.json`;
+            break;
+        case "best":
+            url = `${BASE_URL}/${pageName}/.json`;
+            break;
+        case "popular":
+            url = `${BASE_URL}/r/${pageName}/.json`;
+            break;
+        default:
+            url = `${BASE_URL}/.json`;
+    }
+    
     const result = await fetch(url);
     const json = await result.json();
-    
+
     return json.data.children;
-}
+};
