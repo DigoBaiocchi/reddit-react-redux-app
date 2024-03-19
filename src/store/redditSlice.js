3e912831ccb49a2da99976e79b420d5c2d1992e3
Rodrigo <digo.baiocchi@gmail.com>
2024-03-19 19:47:25 -0400

    Add react-redux and store features
    Installing react-redux and store features to fetchPosts, pageName to
    initialState and selectPosts and selectPageName functions

--- src/store/redditSlice.js
+++ src/store/redditSlice.js
@@ -1,7 +1,9 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { redditPageApi } from "../api/redditApi";
 
 const initialState = {
     posts: [],
+    pageName: 'Home'
 };
 
 const redditSlice = createSlice({
@@ -10,6 +12,9 @@ const redditSlice = createSlice({
     reducers: {
         setPosts(state, action) {
             state.posts = action.payload;
+        },
+        setPageName(state, action) {
+            state.pageName = action.payload;
         }
     }
 });
@@ -17,3 +22,17 @@ const redditSlice = createSlice({
 export const { setPosts } = redditSlice.actions;
 
 export default loadPostsSlice.reducer;
+
+export const fetchPosts = () => async (dispatch, getState) => {
+    try {
+        const state = getState();
+        const { pageName } = state;
+        const posts = await redditPageApi(pageName);
+        dispatch(setPosts(posts));
+    } catch (err) {
+        console.log(err);
+    }
+};
+
+export const selectPosts = (state) => state.reddit.posts;
+export const selectPageName = (state) => state.reddit.pageName;
